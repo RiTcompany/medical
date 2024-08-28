1f22819f49dd5b300e80225d8752161a5c5639e8
pezdqk <pavel.kobzov@gmail.com>
2024-08-28 21:30:35 +0300

    feat: Added Notifiaction routes

--- medical_inventory/settings.py
+++ medical_inventory/settings.py
@@ -47,7 +47,6 @@ INSTALLED_APPS = [
     'client',
     'post',
     'notification',
-    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- notification/views.py
+++ notification/views.py
@@ -3,14 +3,10 @@ from rest_framework.permissions import IsAuthenticated
 from .models import Notification
 from .serializers import NotificationSerializer
 
+
 class NotificationViewSet(viewsets.ModelViewSet):
+    queryset = Notification.objects.all()
     serializer_class = NotificationSerializer
-    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        # Возвращаем уведомления только для текущего пользователя
-        return Notification.objects.filter(recipient=self.request.user)
-
-    def perform_create(self, serializer):
-        # Автоматически указываем получателя уведомления как текущего пользователя
-        serializer.save(recipient=self.request.user)
+        return Notification.objects.filter(recipient_group=self.request.user.groups.all()[0])
