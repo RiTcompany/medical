3a602d0d351c12acfa0c232dfd45785503c4e8fd
pezdqk <pavel.kobzov@gmail.com>
2024-10-09 12:44:02 +0300

    fix: Fixed login routes

--- user/urls.py
+++ user/urls.py
@@ -3,7 +3,9 @@ from .views import *
 
 urlpatterns = [
     path('sign_up', SignUpView.as_view()),
+    path('sign_in', TokenLogin.as_view()),
     path('token/login', TokenLogin.as_view()),
+    path('sign_out', TokenLogout.as_view()),
     path('token/logout', TokenLogout.as_view()),
     path('token/me', TokenMe.as_view()),
     # path('subscriptions', SubscriptionTypeView.as_view()),
